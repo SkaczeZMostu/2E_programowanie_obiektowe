84d25f9df956b752341258848b3716804dc3672c
SkaczeZMostu <szymczok.zoemk@gmail.com>
2023-10-03 15:07:31 +0200

    zadanie 3

---
+++ 03_10_2023/Zadnie3/Zadnie3/Zadnie3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{   
+    int a, b, c;
+    cout << "Podaj 1 bok" << endl;
+    cin >> a;
+    cout << "Podaj 2 bok" << endl;
+    cin >> b;
+    cout << "Podaj 3 bok" << endl;
+    cin >> c;
+
+    if (a + b > c && a + c > b && b + c > a)
+        cout << "da sie zrobic taki trojkat" << endl;
+    else
+        cout << "nie da sie zrobic takiego trojkata" << endl;
+}
+
