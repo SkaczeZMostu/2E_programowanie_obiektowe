d96e74ec2b6644905ee4ff5cb5313352678495ec
SkaczeZMostu <szymczok.zomek@gmail.com>
2023-10-10 14:20:29 +0200

    Zadania z dnia 10.10.2023

---
+++ 10_10_2023/Zad1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    string haslo, login;
+    cout << "Podaj login: " << endl;
+    cin >> login;
+    cout << "Podaj haslo: " << endl;
+    cin >> haslo;
+    if (haslo == "Super123" && login == "admin")
+        cout << "Zalogowales sie na admina" << endl;
+    else
+        cout << "Bledne haslo" << endl;
+}
+
---
+++ 10_10_2023/Zad2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    string haslo, login;
+    do {
+        cout << "Podaj login: " << endl;
+        cin >> login;
+        cout << "Podaj haslo: " << endl;
+        cin >> haslo;
+        if (haslo == "Super123" && login == "admin") {
+            cout << "Zalogowales sie na admina" << endl;
+            break;
+        }
+        else if (haslo == "JestemTak" && login == "user") {
+            cout << "Zalogowales sie na uzytkownika" << endl;
+            break;
+        }
+        else if (haslo == "Swietnie" && login == "user1") {
+            cout << "Zalogowales sie na uzytkownika" << endl;
+            break;
+        }
+        else {
+            cout << "Bledne haslo lub login" << endl;
+            cout << "Sprubuje sieznowu zalogowac " << endl;
+        }
+    } while (true);
+}
+
