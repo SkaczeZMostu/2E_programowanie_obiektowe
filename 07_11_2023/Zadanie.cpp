7e994b9ba7bf4b5134e756e0d492e4be69d203ae
SkaczeZMostu <145669488+SkaczeZMostu@users.noreply.github.com>
2023-11-07 14:46:53 +0100

    Update Zadanie.cpp

--- 07_11_2023/Zadanie.cpp
+++ 07_11_2023/Zadanie.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstdlib>
 #include <time.h>
 #include <ctime>
@@ -9,30 +9,47 @@ int main()
     srand(time(0));
     int number;
     int computer = rand() % 5 + 1;
-    cout << "Podaj liczbe od 1-5(1-nożyce, 2-papier, 3-kamien, 4-jaszczurka, 5-spock)" << endl;
-    cin >> number;
-    while(number == 0 && number > 5){
-        if (number == computer) {
+    int win = 0;
+    int cwin = 0;
+    int draw = 0;
+    
+    while(win != 3){
+        cout << "Podaj liczbe od 1-5(1-nozyce, 2-papier, 3-kamien, 4-jaszczurka, 5-spock)" << endl;
+        cin >> number;
+        if (number == 0 && number > 5) {
+            cout << "Zla liczba " << endl;
+            break;
+        }
+        else if (number == computer) {
             cout << "remis" << endl;
+            draw++;
         }
         else if (number == 1 && computer == 2 || computer == 4) {
             cout << "Wygrales" << endl;
+            win++;
         }
         else if (number == 2 && computer == 3 || computer == 5) {
             cout << "Wygrales" << endl;
+            win++;
         }
         else if (number == 3 && computer == 1 || computer == 4) {
             cout << "Wygrales" << endl;
+            win++;
         }
         else if (number == 4 && computer == 2 || computer == 5) {
             cout << "Wygrales" << endl;
+            win++;
         }
         else if (number == 5 && computer == 1 || computer == 3) {
             cout << "Wygrales" << endl;
+            win++;
         }
         else {
             cout << "przegrales" << endl;
+            cwin++;
         }
+        
+        
     }
-
+    cout << "Wygrales " << win << " razy" << endl;
 }
